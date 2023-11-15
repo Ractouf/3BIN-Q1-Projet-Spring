ed00b3837a28a1e160a33cc2e952324e09c54f9e
francois.vandeputte <francois.vandeputte@student.vinci.be>
2023-11-15 15:20:31 +0100

    feat: Investor microservice

---
+++ investors/src/test/java/ipl/vinci/be/investors/InvestorsApplicationTests.java
@@ -0,0 +1,13 @@
+package ipl.vinci.be.investors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
