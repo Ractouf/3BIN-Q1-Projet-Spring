fd259a542f4160ff205aafb6d332e44a29c1b63f
Damien <lapinskidamien@gmail.com>
2023-12-01 17:01:07 +0100

    lore: comments on all methods

--- order/src/main/java/be/vinci/ipl/order/OrderService.java
+++ order/src/main/java/be/vinci/ipl/order/OrderService.java
@@ -14,14 +14,29 @@ public class OrderService {
     this.repository = repository;
   }
 
+  /**
+   * Create a new order
+   * @param order the order to create
+   */
   public void createOne(Order order) {
     repository.save(order);
   }
 
+  /**
+   * Get an order from it's guid
+   * @param guid the id of the order
+   * @return the order or null if it doesn't exist (which allows the controller to throw a 404 error)
+   */
   public Order readOne(String guid) {
     return repository.findById(guid).orElse(null);
   }
 
+  /**
+   * Update the filled value of an order
+   * @param guid the id of the order
+   * @param filled the amount to add
+   * @return returns the order or null if it doesn't exist
+   */
   public Order changeFilled(String guid, int filled) {
     Order order = repository.findById(guid).orElse(null);
     if(order != null){
@@ -35,10 +50,21 @@ public class OrderService {
     return order;
   }
 
+  /**
+   * Get the orders of an investor
+   * @param username the investor
+   * @return all the investor's orders
+   */
   public Iterable<Order> readOwner(String username) {
     return repository.findByOwner(username).orElse(null);
   }
 
+  /**
+   * Get open orders of a specific ticker and side
+   * @param ticker the ticker to find
+   * @param side side to find
+   * @return the orders or empty if none are found
+   */
   public Iterable<Order> readTicker(String ticker, OrderSide side){
     Iterable<Order> orders = repository.findByTickerAndSide(ticker, side);
     ArrayList<Order> results = new ArrayList<>();
