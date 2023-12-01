f0848410c0427169c6ac831b1275fc931e4e91c4
Damien <lapinskidamien@gmail.com>
2023-12-01 16:32:12 +0100

    lore: comments on all methods

--- wallet/src/main/java/be/vinci/ipl/wallet/WalletService.java
+++ wallet/src/main/java/be/vinci/ipl/wallet/WalletService.java
@@ -14,6 +14,12 @@ public class WalletService {
         this.repository = repository;
     }
 
+    /**
+     * Get the value of an investor's entire wallet
+     * @param owner the investor
+     * @throws NotFoundException when the investor isn't found
+     * @return the net-worth of an investor
+     */
     public double netWorth(String owner) throws NotFoundException {
         if (!repository.existsByUsername(owner))
             throw new NotFoundException();
@@ -26,6 +32,12 @@ public class WalletService {
         return result;
     }
 
+    /**
+     * Get a list of an investor's open positions
+     * @param owner the investor
+     * @throws NotFoundException when the investor isn't found
+     * @return a list of an investor's open positions
+     */
     public Iterable<PositionUser> positions(String owner) throws NotFoundException {
         if (!repository.existsByUsername(owner))
             throw new NotFoundException();
@@ -38,6 +50,13 @@ public class WalletService {
         return result;
     }
 
+    /**
+     * Add a list of positions to an investor's wallet
+     * @param owner the investor
+     * @param ajouts the list of positions to add to the wallet
+     * @return the wallet after the changes
+     * we decided not to throw a 404 error since there wasn't any other way to create a wallet
+     */
     public Iterable<PositionUser> addPosition(String owner, Iterable<Position> ajouts) {
         Iterable<PositionUser> positions = repository.findAllByUsername(owner);
         boolean enter;
